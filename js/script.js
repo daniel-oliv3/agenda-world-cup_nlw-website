a658115897bc01c63fa4a0a3bde7117ef249d4e1
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-09 09:58:28 -0300

    Create card js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 /* JavaScript */
- 
+
+function createCard(){
+  return `
+    <div class="card">
+			<h2>24/11 <span>Quinta</span></h2>
+			<ul>
+				<li>
+					<img src="img/teams/icon=brazil.svg" alt="Bandeira do Brasil">
+					<strong>07:00</strong>
+					<img src="img/teams/icon=serbia.svg" alt="Bandeira da Sérvia">
+				</li>
+			</ul>
+		</div>
+  `;
+}
+
+
+document.querySelector("#app").innerHTML = `
+  <header>
+		<img src="img/assets/logo.svg" alt="Logo">
+	</header>
+	<main class="cards">
+		${createCard()}
+    ${createCard()}
+    ${createCard()}
+	</main>
+ `
 
 
 
